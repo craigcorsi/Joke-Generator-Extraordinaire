e916643a43b6ed051b03cc512784ff5e15b3589f
Craig <corsi007@umn.edu>
2018-04-26 18:57:14 -0500

    bootstrap templates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
+var apiKey = '0reA09JpgnmshGI6Z2Sxl6usmjoWp1aEIV4jsn1ImdkLbThVb6';
+var testingKey = 'qINz8NnrVPmshq9TFlEf8RsC0frhp1CIQeRjsnY5jW3G47kS7P';
+// var unirest = require('unirest');
+
+requirejs(["unirest"], function(util) {
+    console.log('test');
+});
+
+unirest.get("https://wordsapiv1.p.mashape.com/words/soliloquy")
+.header("X-Mashape-Key", "<" + apiKey + ">")
+
+rest.get()
+
 console.log('hello');
