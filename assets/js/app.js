8b18a7fc55dce5dc1bdfa037773f130d3d1fb2a9
Craig <corsi007@umn.edu>
2018-04-26 19:21:17 -0500

    working WordsAPI call

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,18 @@ var apiKey = '0reA09JpgnmshGI6Z2Sxl6usmjoWp1aEIV4jsn1ImdkLbThVb6';
 var testingKey = 'qINz8NnrVPmshq9TFlEf8RsC0frhp1CIQeRjsnY5jW3G47kS7P';
 // var unirest = require('unirest');
 
-requirejs(["unirest"], function(util) {
-    console.log('test');
+var searchTerm = 'soliloquy'
+
+$(document).ready(function () {
+    $.ajax({
+        url: "https://wordsapiv1.p.mashape.com/words/" + searchTerm,
+        headers: {
+            "X-Mashape-Key": apiKey
+        }
+    }).then(function (response) {
+        console.log(response);
+    });
+
 });
 
-unirest.get("https://wordsapiv1.p.mashape.com/words/soliloquy")
-.header("X-Mashape-Key", "<" + apiKey + ">")
 
-rest.get()
-
-console.log('hello');
