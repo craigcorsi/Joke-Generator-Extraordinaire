a3de93b8e06a18dc7be2c3aa829f66726d134c3b
Craig <corsi007@umn.edu>
2018-04-26 21:07:12 -0500

    display synonyms and tyeOf words

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,37 @@ var testingKey = 'qINz8NnrVPmshq9TFlEf8RsC0frhp1CIQeRjsnY5jW3G47kS7P';
 
 var searchTerm;
 
+function pullRelatedWords(term) {
+    var cell = $('<div>').css({ 'border' : '1px solid black' });
+    cell.append("<p>The definition of this word is: " + term.definition + ".</p>")
+
+    cell.append("<p> Here are a few related words: </p>");
+    var l = term.synonyms.length;
+    for (var i=0; i < l; i++) {
+        cell.append(term.synonyms[i] + "<br>");
+    }
+    cell.append("<br>");
+
+
+    cell.append("<p> This word is a type of: </p>");
+    var l = term.typeOf.length;
+    for (var i=0; i < l; i++) {
+        cell.append(term.typeOf[i] + "<br>");
+    }
+    cell.append("<br>");
+
+    $('#search-results-here').append(cell);
+    cell.append("<p> This word is part of a: </p>");
+    var l = term.partOf.length;
+    for (var i=0; i < l; i++) {
+        cell.append(term.typeOf[i] + "<br>");
+    } 
+    cell.append("<br>");
+
+
+    $('#search-results-here').append(cell);
+}
+
 $(document).ready(function () {
 
     $('#button-submit-term').on('click', function() {
@@ -15,14 +46,17 @@ $(document).ready(function () {
         $.ajax({
             url: "https://wordsapiv1.p.mashape.com/words/" + searchTerm,
             headers: {
-                "X-Mashape-Key": apiKey
+                "X-Mashape-Key": apiKey,
             },
+            crossDomain: true,
             xhrFields: {
                 withCredentials: true
             }
         }).then(function (response) {
-            console.log(response);
-            $('#search-results-here').append(JSON.stringify(response));
+            term = response.results[0];
+            console.log(term);
+            pullRelatedWords(term);
+            // $('#search-results-here').append(JSON.stringify(response));
         });
     })
 
